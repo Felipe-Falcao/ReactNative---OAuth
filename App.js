92ff7299b88ae5ff570cf03fe122ae236bf97179
fsfalcao <fsfalcao@AGIN110>
2021-06-01 12:56:36 -0300

    Other auth

--- App.js
+++ App.js
@@ -1,41 +1,208 @@
-
-import React, { Component } from 'react';
+import React, { useState, useCallback, useMemo } from 'react';
+import { UIManager, Alert } from 'react-native';
+import { authorize, refresh, revoke, prefetchConfiguration } from 'react-native-app-auth';
 import {
   SafeAreaView,
   ScrollView,
   StatusBar,
   StyleSheet,
   Text,
+  Button,
+  View,
   TouchableOpacity,
 } from 'react-native';
-import { authorize } from 'react-native-app-auth';
-import AuthProvider from './src/Auth.js'
+import AuthContainer from './src/Auth'
 
-const config = {
-  issuer: 'https://accounts.google.com',
-  clientId: '83849151947-lla6l5mrep1fsn0bpj8gvo098e4t6ldt.apps.googleusercontent.com',
-  // clientSecret: 'Authentication:Google:ClientSecret" "M_GnWZ1VrBtP7KQXrmwk-xHu',
-  redirectUrl: 'com.googleusercontent.apps.83849151947-lla6l5mrep1fsn0bpj8gvo098e4t6ldt:/oauth2redirect/google',
-  scopes: ['openid', 'profile']
+// const configs = {
+//   google: {
+//     issuer: 'https://accounts.google.com',
+//     clientId: '700126173685-ri2hce8g5031m4hdfrhl2uq4blb8sctj.apps.googleusercontent.com',
+//     redirectUrl: 'com.googleusercontent.apps.700126173685-ri2hce8g5031m4hdfrhl2uq4blb8sctj:/oauth2redirect/google',
+//     scopes: ['openid', 'profile'],
+//     // warmAndPrefetchChrome: true,
+//   }
+// };
+
+const configs = {
+  identityserver: {
+    issuer: 'https://demo.identityserver.io',
+    clientId: 'interactive.public',
+    redirectUrl: 'io.identityserver.demo:/oauthredirect',
+    additionalParameters: {},
+    scopes: ['openid', 'profile', 'email', 'offline_access'],
+
+    // serviceConfiguration: {
+    //   authorizationEndpoint: 'https://demo.identityserver.io/connect/authorize',
+    //   tokenEndpoint: 'https://demo.identityserver.io/connect/token',
+    //   revocationEndpoint: 'https://demo.identityserver.io/connect/revoke'
+    // }
+  },
+  auth0: {
+    // From https://openidconnect.net/
+    issuer: 'https://samples.auth0.com',
+    clientId: 'kbyuFDidLLm280LIwVFiazOqjO3ty8KH',
+    redirectUrl: 'https://openidconnect.net/callback',
+    additionalParameters: {},
+    scopes: ['openid', 'profile', 'email', 'phone', 'address'],
+
+    // serviceConfiguration: {
+    //   authorizationEndpoint: 'https://samples.auth0.com/authorize',
+    //   tokenEndpoint: 'https://samples.auth0.com/oauth/token',
+    //   revocationEndpoint: 'https://samples.auth0.com/oauth/revoke'
+    // }
+  },
+  google: {
+    issuer: 'https://accounts.google.com',
+    clientId: '700126173685-ri2hce8g5031m4hdfrhl2uq4blb8sctj.apps.googleusercontent.com',
+    redirectUrl: 'com.googleusercontent.apps.700126173685-ri2hce8g5031m4hdfrhl2uq4blb8sctj:/oauth2redirect/google',
+    scopes: ['openid', 'profile'],
+    // warmAndPrefetchChrome: true,
+  }
 };
 
-export default class App extends Component {
-  state = {
-    token: ''
-  }
 
-  getGoogle = async () => {
-    const authState = await authorize(config)
-    // console.log(authState)
-  }
+const defaultAuthState = {
+  hasLoggedInOnce: false,
+  provider: '',
+  accessToken: '',
+  accessTokenExpirationDate: '',
+  refreshToken: ''
+};
 
-  render() {
-    return (
-      <AuthProvider>
-        <TouchableOpacity onPress={this.getGoogle}>
-          <Text>Google</Text>
-        </TouchableOpacity>
-      </AuthProvider>
-    );
-  }
+const App = () => {
+  const [authState, setAuthState] = useState(defaultAuthState);
+
+  React.useEffect(() => {
+    prefetchConfiguration({
+      warmAndPrefetchChrome: true,
+      ...configs.identityserver
+    });
+  }, []);
+
+  const handleAuthorize = useCallback(
+    async provider => {
+      try {
+        const config = configs[provider];
+        const newAuthState = await authorize(config);
+
+        setAuthState({
+          hasLoggedInOnce: true,
+          provider: provider,
+          ...newAuthState
+        });
+      } catch (error) {
+        Alert.alert('Falha ao realizar o Login', error.message);
+      }
+    },
+    [authState]
+  );
+
+  const handleRefresh = useCallback(async () => {
+    try {
+      const config = configs[authState.provider];
+      const newAuthState = await refresh(config, {
+        refreshToken: authState.refreshToken
+      });
+
+      setAuthState(current => ({
+        ...current,
+        ...newAuthState,
+        refreshToken: newAuthState.refreshToken || current.refreshToken
+      }))
+
+    } catch (error) {
+      Alert.alert('Failed to refresh token', error.message);
+    }
+  }, [authState]);
+
+  const handleRevoke = useCallback(async () => {
+    try {
+      const config = configs[authState.provider];
+      await revoke(config, {
+        tokenToRevoke: authState.accessToken,
+        sendClientId: true
+      });
+
+      setAuthState({
+        provider: '',
+        accessToken: '',
+        accessTokenExpirationDate: '',
+        refreshToken: ''
+      });
+    } catch (error) {
+      Alert.alert('Failed to revoke token', error.message);
+    }
+  }, [authState]);
+
+  const showRevoke = useMemo(() => {
+    if (authState.accessToken) {
+      const config = configs[authState.provider];
+      if (config.issuer || config.serviceConfiguration.revocationEndpoint) {
+        return true;
+      }
+    }
+    return false;
+  }, [authState]);
+
+  return (
+    <AuthContainer>
+      {!!authState.accessToken ? (
+        <View>
+          <Text style={{ backgroundColor: '#CECECE' }}>accessToken</Text>
+          <Text>{authState.accessToken}</Text>
+          <Text style={{ backgroundColor: '#CECECE' }}>accessTokenExpirationDate</Text>
+          <Text>{authState.accessTokenExpirationDate}</Text>
+          <Text style={{ backgroundColor: '#CECECE' }}>refreshToken</Text>
+          <Text>{authState.refreshToken}</Text>
+          <Text style={{ backgroundColor: '#CECECE' }}>scopes</Text>
+          <Text>{authState.scopes.join(', ')}</Text>
+        </View>
+      ) : (
+        <Text>{authState.hasLoggedInOnce ? 'Goodbye.' : 'Hello, stranger.'}</Text>
+      )}
+
+      <View>
+        {!authState.accessToken ? (
+          <>
+            <TouchableOpacity
+              style={{ backgroundColor: '#CECECE', padding: 10, alignItems: 'center', margin: 10 }}
+              onPress={() => handleAuthorize('identityserver')}
+            >
+              <Text>Authorize IdentityServer</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={{ backgroundColor: '#CECECE', padding: 10, alignItems: 'center', margin: 10 }}
+              onPress={() => handleAuthorize('auth0')}
+            >
+              <Text>Authorize Auth0</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={{ backgroundColor: 'green', padding: 10, alignItems: 'center', margin: 10 }}
+              onPress={() => handleAuthorize('google')}
+            >
+              <Text style={{ color: 'white' }}>Google</Text>
+            </TouchableOpacity>
+          </>
+        ) : null}
+        {!!authState.refreshToken ? (
+          <TouchableOpacity
+            style={{ backgroundColor: '#CECECE', padding: 10, alignItems: 'center', margin: 10 }}
+            onPress={handleRefresh}
+          >
+            <Text>Refresh</Text>
+          </TouchableOpacity>
+        ) : null}
+        {showRevoke ? (
+          <TouchableOpacity
+            style={{ backgroundColor: '#CECECE', padding: 10, alignItems: 'center', margin: 10 }}
+            onPress={handleRevoke}
+          >
+            <Text>Revoke</Text>
+          </TouchableOpacity>
+        ) : null}
+      </View>
+    </AuthContainer>
+  );
 }
+
+export default App;
